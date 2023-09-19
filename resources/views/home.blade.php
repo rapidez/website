e85308ad064010baf6c9fc20b33b70f141f221c3
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-09-19 09:39:19 +0200

    Update tailwind & add image text template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
             ['src' => 'reactive-search.svg', 'alt' => 'Reactive Search', 'href' => 'https://opensource.appbase.io/reactivesearch/vue', 'class' => 'h-10'],
         ],
     ])
+    @include('partials.image-text')
     @include('partials.features')
     @include('partials.seperator-logo')
     @include('partials.faq')
