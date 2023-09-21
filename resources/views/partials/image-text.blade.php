2bb86c2289006c606f9cc571ccc0b041637727d7
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-09-21 08:10:52 +0200

    Fix out of container margin

--- resources/views/partials/image-text.blade.php
+++ resources/views/partials/image-text.blade.php
@@ -1,7 +1,7 @@
 <div class="mx-auto mb-12 flex max-w-7xl flex-col gap-20 px-6 md:mb-32 md:gap-28">
     <div class="grid items-center gap-x-60 gap-y-6 md:grid-cols-2">
         <div class="flex flex-col items-start max-md:order-last">
-            <span class="text-sm text-green-600">
+            <span class="text-sm text-secondary-100">
                 Why did we start with Rapidez
             </span>
             <h2 class="mt-2 text-4xl font-semibold text-heading">
@@ -11,25 +11,25 @@
                 Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula
             </span>
         </div>
-        <div class="relative mr-[calc(100%-100vw)] max-md:ml-20">
+        <div class="relative mr-[calc(50%-50vw)] max-md:ml-20">
             <x-icon-angry class="absolute left-0 top-1/2 aspect-square h-52 -translate-y-1/2 translate-x-[-145px] text-gray-100 md:h-72 md:translate-x-[-175px]" />
             <div class="flex flex-col rounded-2xl drop-shadow-toolbar">
                 <div class="relative flex gap-3 rounded-t-2xl bg-white p-3 md:px-4 md:py-3.5">
-                    @foreach (['hover:bg-red-500', 'hover:bg-amber-500', 'hover:bg-green-500'] as $color)
+                    @foreach (['hover:bg-red-500', 'hover:bg-amber-500', 'hover:bg-secondary-100'] as $color)
                         <div @class([
                             'aspect-square h-4 rounded-full bg-gray-100 transition md:h-5',
                             $color,
                         ])></div>
                     @endforeach
                 </div>
-                <img src="/img/code.png" alt="" class="relative mr-auto h-[360px] rounded-b-2xl object-cover object-left-top md:h-[720px]" />
+                <img src="/img/code.png" alt="" class="relative mr-auto h-[360px] w-full rounded-b-2xl object-cover object-left-top md:h-[720px]" />
             </div>
         </div>
     </div>
 
     <div class="grid items-center gap-x-60 gap-y-6 md:grid-cols-2">
         <div class="order-last flex flex-col items-start">
-            <span class="text-sm text-green-600">
+            <span class="text-sm text-secondary-100">
                 Easy to use
             </span>
             <h2 class="mt-2 text-4xl font-semibold text-heading">
@@ -59,18 +59,18 @@
                 <a href="#" class="flex items-center gap-3">
                     <x-icon-rocket-sm class="h-5 text-gray-400" />
                     <span>
-                        <strong class="text-heading">15</strong>
+                        <strong class="text-heading">2</strong>
                         <span class="text-inactive">minutes to install</span>
                     </span>
                     <x-heroicon-o-external-link class="h-4 text-inactive" />
                 </a>
             </div>
         </div>
-        <div class="relative max-md:ml-20 max-md:mr-[calc(100%-100vw)] md:ml-[calc(100%-100vw)]">
+        <div class="relative max-md:ml-20 max-md:mr-[calc(50%-50vw)] md:ml-[calc(50%-50vw)]">
             <x-icon-rocket class="absolute max-md:left-0 h-[300px] translate-x-[-240px] text-gray-100 max-md:top-1/2 max-md:-translate-y-1/2 md:bottom-0 md:right-0 md:h-[570px] md:translate-x-[175px]" />
             <div class="flex flex-col rounded-2xl drop-shadow-toolbar">
                 <div class="relative flex gap-3 rounded-t-2xl bg-white p-3 md:px-4 md:py-3.5">
-                    @foreach (['hover:bg-red-500', 'hover:bg-amber-500', 'hover:bg-green-500'] as $color)
+                    @foreach (['hover:bg-red-500', 'hover:bg-amber-500', 'hover:bg-secondary-100'] as $color)
                         <div @class([
                             'aspect-square h-4 rounded-full bg-gray-100 transition md:h-5',
                             $color,
