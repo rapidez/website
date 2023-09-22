7d66871e9799d67fad3f57f66d18b1b8450716d4
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-09-22 12:58:41 +0200

    Feedback fixe

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,21 +7,20 @@
     <div class="relative py-16">
         <div class="absolute inset-x-0 bottom-0 h-96 bg-gray-100"></div>
         <div class="container relative mx-auto max-w-7xl px-6">
-            <h1 class="text-3xl font-extrabold text-secondary-100 mb-5">Blog</h1>
             @php($blogs = File::files(resource_path('views/content/blogs')))
 
-            <div class="flex gap-8">
+            <div class="flex gap-8 max-lg:flex-col">
                 @foreach (array_slice($blogs, 0, 1) as $file)
                     @php($name = Str::of($file->getFilename())->replace('.blade.php', ''))
-                    <a href="/blog/{{ $name }}" class="flex w-full [&>*>div>h3]:text-4xl [&>*>img]:h-[416px]">
+                    <a href="/blog/{{ $name }}" class="flex w-full [&>*>div>h3]:text-3xl lg:[&>*>div>h3]:text-4xl lg:[&>*>img]:h-[416px]">
                         @include('content.blogs.' . $name, ['overview' => true])
                     </a>
                 @endforeach
                 @if ($blogs > 1)
-                    <div class="flex w-2/3 flex-col gap-8">
+                    <div class="flex flex-col gap-8 lg:w-2/3">
                         @foreach (array_slice($blogs, 1, 2) as $file)
                             @php($name = Str::of($file->getFilename())->replace('.blade.php', ''))
-                            <a href="/blog/{{ $name }}" class="flex h-48 [&>*>img]:aspect-square [&>*]:flex-row [&>*]:gap-8">
+                            <a href="/blog/{{ $name }}" class="flex lg:h-48 lg:[&>*>img]:aspect-square [&>*]:gap-8 lg:[&>*]:flex-row">
                                 @include('content.blogs.' . $name, ['overview' => true])
                             </a>
                         @endforeach
@@ -29,7 +28,7 @@
                 @endif
             </div>
             @if ($blogs > 3)
-                <div class="mt-16 grid grid-cols-2 gap-x-8 gap-y-16 border-t pt-16">
+                <div class="mt-16 grid gap-x-8 gap-y-16 lg:grid-cols-2 lg:border-t lg:pt-16">
                     @foreach (array_slice($blogs, 3) as $file)
                         @php($name = Str::of($file->getFilename())->replace('.blade.php', ''))
                         <a href="/blog/{{ $name }}" class="flex">
--- resources/views/components/blog.blade.php
+++ resources/views/components/blog.blade.php
@@ -4,9 +4,9 @@
         <img class="rounded-lg object-cover" src="{{ $image }}" alt="" />
         <div class="flex flex-col self-center">
             <span class="text-sm text-secondary-100">{{ $category }}</span>
-            <h3 class="mt-2 text-xl font-semibold text-cyan-900">{{ $title }}</h3>
+            <h3 class="mt-2 text-2xl font-semibold text-cyan-900">{{ $title }}</h3>
             @isset($short_description)
-                <span class="mt-4 line-clamp-2 text-sm text-inactive">{{ $short_description }}</span>
+                <span class="mt-4 line-clamp-4 text-sm text-inactive lg:line-clamp-2">{{ $short_description }}</span>
             @endisset
         </div>
     </div>
@@ -14,7 +14,7 @@
     @section('title', $title . ' - Rapidez')
     <div class="prose flex max-w-none flex-col">
         <span class="text-sm text-secondary-100">{{ $category }}</span>
-        <h3 class="mt-2 mb-0 text-4xl font-semibold text-cyan-900">{{ $title }}</h3>
+        <h3 class="mb-0 mt-2 text-4xl font-semibold text-cyan-900">{{ $title }}</h3>
         @isset($slot)
             <article>{{ $slot }}</article>
         @endisset
