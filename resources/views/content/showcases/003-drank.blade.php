89c34014bd19ed0ab61effb5d80fcfcbe5789194
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-09-22 13:41:57 +0200

    Apply to everything

--- resources/views/content/showcases/003-drank.blade.php
+++ resources/views/content/showcases/003-drank.blade.php
@@ -1,10 +1,10 @@
 <x-showcase title="Drank" subtitle="The first Rapidez webshop in production!" image="/img/ranzijn.png">
     <p>Drank.nl originally started out of passion for shaking cocktails in various cocktail bars and attending liquor tastings. Today, they are true experts in the field of alcoholic beverages, sharing their knowledge and experience on Drank.nl, where everyone can easily order their favorite drinks for a good price and with friendly service. In addition to the online store, they also have a physical store, "De Boetiek," located in The Hague, where visitors are welcome to come and smell and taste their selection of drinks. Drank.nl is an Multistore B2C webshop within the Barrel Brothers installation.</p>
     <x-slot name="brands">
-        <x-brand><img src="/img/brands/amasty.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/mageprince.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/mirasvit.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/smile.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/dog.png" alt=""/></x-brand>
+        <x-brand><img src="/img/laravel.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/hetzner.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/vue.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/digitalocean.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/tailwind-css.svg" alt=""/></x-brand>
     </x-slot>
 </x-showcase>
--- resources/views/content/showcases/005-veldmanslijptechniek.blade.php
+++ resources/views/content/showcases/005-veldmanslijptechniek.blade.php
@@ -1,10 +1,10 @@
 <x-showcase title="Veldman Slijptechniek" subtitle="The first Rapidez webshop in production!" image="/img/ranzijn.png">
     <p>Veldman Slijptechniek is a company with over 50 years of experience in supplying, sharpening and producing cutting tools. They supply a large number of industries with cutting tools, including the staircase industry, interior construction, timber factories, kitchen and cabinet factories, contractors, furniture makers, panel builders, and wood, metal and plastic processing industries. Veldman Slijptechniek has an Rapidez B2B webshop through which they serve their customers. Veldmanslijptechniek.nl has awesome features like tier pricing and custom search rules!</p>
     <x-slot name="brands">
-        <x-brand><img src="/img/brands/amasty.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/mageprince.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/mirasvit.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/smile.png" alt=""/></x-brand>
-        <x-brand><img src="/img/brands/dog.png" alt=""/></x-brand>
+        <x-brand><img src="/img/laravel.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/hetzner.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/vue.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/digitalocean.svg" alt=""/></x-brand>
+        <x-brand><img src="/img/tailwind-css.svg" alt=""/></x-brand>
     </x-slot>
 </x-showcase>
