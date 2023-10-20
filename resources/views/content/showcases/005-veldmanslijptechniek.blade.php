a40c4d9a767f9e57e3a8e61324edf0e18b6e1fad
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-10-20 15:14:25 +0200

    Add real content

--- resources/views/content/showcases/005-veldmanslijptechniek.blade.php
+++ resources/views/content/showcases/005-veldmanslijptechniek.blade.php
@@ -1,4 +1,4 @@
-<x-showcase title="Veldman Slijptechniek" subtitle="The first Rapidez webshop in production!" image="/img/ranzijn.png">
+<x-showcase title="Veldman Slijptechniek" subtitle="Cutting edge tools" image="/img/cases/veldman.png">
     <p>Veldman Slijptechniek is a company with over 50 years of experience in supplying, sharpening and producing cutting tools. They supply a large number of industries with cutting tools, including the staircase industry, interior construction, timber factories, kitchen and cabinet factories, contractors, furniture makers, panel builders, and wood, metal and plastic processing industries. Veldman Slijptechniek has an Rapidez B2B webshop through which they serve their customers. Veldmanslijptechniek.nl has awesome features like tier pricing and custom search rules!</p>
     <x-slot name="brands">
         <x-brand><img src="/img/laravel.svg" alt=""/></x-brand>
--- resources/views/partials/partners.blade.php
+++ resources/views/partials/partners.blade.php
@@ -11,69 +11,88 @@
         <x-package.group checked>
             <x-slot name="label" class="rounded-l-lg">Payments</x-slot>
             <div class="grid gap-x-5 gap-y-6 md:grid-cols-3">
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://www.pay.nl" category="Payment Provider" title="Seamless Payments">
                     <x-slot name="logo">
                         <x-icon-pay />
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    All-in-one payment provider. Insights, fast transactions and easy installition.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://www.mollie.com" category="Payment Provider" title="Payments in Europe">
                     <x-slot name="logo">
                         <x-icon-mollie />
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    The payment provider for every kind of company. Widely used by many different customers.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://www.multisafepay.com" category="Payment Provider" title="Powerfull provider">
                     <x-slot name="logo">
                         <x-icon-multisafepay />
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    Smart payment solutions for many different kinds of customers.
                 </x-package.card>
             </div>
         </x-package.group>
+
         <x-package.group>
             <x-slot name="label">Frameworks</x-slot>
             <div class="grid gap-x-5 gap-y-6 md:grid-cols-3">
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://www.smile.eu" category="Framework" title="Store locator">
                     <x-slot name="logo">
-                        <x-icon-pay />
+                        <img src="/img/smile.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    A professional way of displaying the locations of your stores in your Magento webshop.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://strapi.io/" category="Framework" title="Headless CMS">
                     <x-slot name="logo">
-                        <x-icon-mollie />
+                        <img src="/img/strapi.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    Build to use headless. Super fast and easily build customisable APIs, manage and distribute from anywhere.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://statamic.com/" category="Framework" title="Powerfull CMS">
                     <x-slot name="logo">
-                        <x-icon-multisafepay />
+                        <img src="/img/statamic.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    A Larvel based powerfull and super flexible CMS. Many different options and a versatile marketplace for addons.
                 </x-package.card>
             </div>
         </x-package.group>
         <x-package.group>
             <x-slot name="label" class="rounded-r-lg">Packages</x-slot>
             <div class="grid gap-x-5 gap-y-6 md:grid-cols-3">
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://amasty.com/" category="Package" title="Magento Solutions">
                     <x-slot name="logo">
-                        <x-icon-pay />
+                        <img src="/img/amasty.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    Has multiple solutions for making Magento a better E-commerce experience and giving your shop an upgrade.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://elgentos.nl/" category="Package" title="Server Side Analytics">
                     <x-slot name="logo">
-                        <x-icon-mollie />
+                        <img src="/img/elgentos.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    Solve the problem of discrepancies between Magento revenue reports and the revenue reports in Google Analytics.
                 </x-package.card>
-                <x-package.card href="#" category="Payment Provider" title="Seamless Payments">
+                <x-package.card target="_blank" href="https://www.mageplaza.com/" category="Package" title="Blog extension">
                     <x-slot name="logo">
-                        <x-icon-multisafepay />
+                        <img src="/img/mageplaza.png" alt=""/>
                     </x-slot>
-                    No, Vue is only used for some functional frontend components like the cart. All Vue components are "renderless"
+                    A must-have tools for every store owner who wants to manage and posts blogs on their site
+                </x-package.card>
+                <x-package.card target="_blank" href="https://mageprince.com/" category="Package" title="FAQ Module">
+                    <x-slot name="logo">
+                        <img src="/img/mageprince.png" alt=""/>
+                    </x-slot>
+                    Create a dedicated FAQ page. With a collapse/expand feature it is a user-friendly FAQ module.
+                </x-package.card>
+                <x-package.card target="_blank" href="https://mirasvit.com/" category="Package" title="Magento Extensions">
+                    <x-slot name="logo">
+                        <img src="/img/mirasvit.png" alt=""/>
+                    </x-slot>
+                    Use different Magento extensions like Advanced Seo Suite and Knowledge Base.
+                </x-package.card>
+                <x-package.card target="_blank" href="https://snow.dog/" category="Package" title="Menu Module">
+                    <x-slot name="logo">
+                        <img src="/img/snowdog.png" alt=""/>
+                    </x-slot>
+                    A versatile menu module for Magento that can be used for a variety of possibilities.
                 </x-package.card>
             </div>
         </x-package.group>
