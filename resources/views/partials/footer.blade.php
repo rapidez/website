74e91bbe6d392525d493429b15590c54e84e8904
Kevin Meijer <kevin@justbetter.nl>
2024-01-10 11:18:23 +0100

    RM-76 - Added navigation

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,36 +1,14 @@
+@php($menuItems = \Statamic\Statamic::tag('nav:main_navigation')->fetch())
 <footer class="bg-white border-t border-gray-200">
     <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8 mb-6">
         <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-            <div class="px-5 py-2">
-                <a href="/#features" class="text-lg text-gray-500 hover:text-gray-900">
-                    Features
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="/#faq" class="text-lg text-gray-500 hover:text-gray-900">
-                    FAQ
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="/showcases" class="text-lg text-gray-500 hover:text-gray-900">
-                    Showcases
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="/#partners" class="text-lg text-gray-500 hover:text-gray-900">
-                    Partners
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="/blog" class="text-lg text-gray-500 hover:text-gray-900">
-                    Blog
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="http://docs.rapidez.io/" target="_blank" class="text-lg text-gray-500 hover:text-gray-900">
-                    Docs
-                </a>
-            </div>
+            @foreach ($menuItems as $item)
+                <div class="px-5 py-2">
+                    <a href="{{ $item['url'] }}" target="{{ isset($item['open_in_new_tab']) && $item['open_in_new_tab'] ? '_blank' : '_self' }}" class="text-lg text-gray-500 hover:text-gray-900">
+                        {{ $item['title'] }}
+                    </a>
+                </div>
+            @endforeach
         </nav>
         <div class="flex flex-wrap justify-center items-center mt-8">
             <a class="block mr-7 text-primary-200 opacity-50 hover:opacity-100 transition duration-100 ease-in-out" href="https://twitter.com/rapidez_io">
