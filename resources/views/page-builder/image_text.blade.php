eab5f70621afeba04fa585bebdc05531c8b8c977
Roy Duineveld <royduineveld@gmail.com>
2024-01-19 14:18:26 +0100

    Cache invalidation

--- resources/views/page-builder/image_text.blade.php
+++ resources/views/page-builder/image_text.blade.php
@@ -4,23 +4,17 @@
             @if($content ?? false)
                 <div class="flex flex-col items-start max-md:order-last">
                     @foreach($content as $contentSet)
-                        @includeIf('partials.content.' . $contentSet['type'], $contentSet)
+                        @includeFirstSafe(['partials.content.' . $contentSet['type']], $contentSet)
                     @endforeach
                 </div>
             @endif
-            <div class="relative mr-[calc(50%-50vw)] max-md:ml-20">
-                @if($background_icon?->value()?->value() ?? '')
-                    @if(($background_icon?->value()?->value() ?? '') === 'angry-face')
-                        <x-icon-angry class="absolute left-0 top-1/2 aspect-square h-52 -translate-y-1/2 translate-x-[-145px] text-gray-100 md:h-72 md:translate-x-[-275px]" />
-                    @else
-                        <x-icon-rocket class="absolute left-0 top-1/2 aspect-square h-52 -translate-y-1/2 translate-x-[-145px] text-gray-100 md:h-72 md:translate-x-[-275px]" />
-                    @endif
-                @endif
+            <div class="relative md:mr-[calc(50%-50vw)] md:ml-20">
                 <div class="flex flex-col rounded-2xl drop-shadow-toolbar">
+                    @php($class = 'relative mr-auto h-[360px] w-full rounded-bl-2xl object-cover object-left-top md:h-[720px] md:rounded-b-2xl')
                     @if(($media?->value()?->isImage() ?? false) || ($media?->value()?->isSvg() ?? false))
-                        @responsive($media->value(), ['class' => 'relative mr-auto h-[360px] w-full rounded-bl-2xl object-cover object-left-top md:h-[720px] md:rounded-b-2xl'])
+                        @responsive($media->value(), compact('class'))
                     @elseif(($media?->value()?->isVideo() ?? false) && ($media?->value()?->url() ?? false))
-                        <video src="{{ $media->value()->url() }}" loop autoplay muted class="relative mr-auto h-[360px] w-full rounded-bl-2xl object-cover object-left-top md:h-[720px] md:rounded-b-2xl" />
+                        <video src="{{ $media->value()->url() }}" loop autoplay muted :class="$class" />
                     @endif
                 </div>
             </div>
@@ -34,19 +28,13 @@
                     @endforeach
                 </div>
             @endif
-            <div class="relative max-md:ml-20 max-md:mr-[calc(50%-50vw)] md:ml-[calc(50%-50vw)]">
-                @if($background_icon?->value()?->value() ?? '')
-                    @if(($background_icon?->value()?->value() ?? '') === 'angry-face')
-                        <x-icon-angry class="absolute left-0 top-1/2 aspect-square h-52 -translate-y-1/2 translate-x-[-145px] text-gray-100 md:h-72 md:translate-x-[-175px]" />
-                    @else
-                        <x-icon-rocket class="absolute h-[300px] -translate-x-56 text-gray-100 max-md:left-0 max-md:top-1/2 max-md:-translate-y-1/2 md:bottom-0 md:right-0 md:h-[570px] md:translate-x-[175px]" />
-                    @endif
-                @endif
+            <div class="relative md:ml-20 md:mr-[calc(50%-50vw)] md:ml-[calc(50%-50vw)]">
                 <div class="flex flex-col rounded-2xl drop-shadow-toolbar">
+                    @php($class = 'max-md:mr-auto relative rounded-bl-2xl md:object-cover md:object-left-top md:ml-auto md:h-[720px] md:rounded-b-2xl md:object-right-top')
                     @if(($media?->value()?->isImage() ?? false) || ($media?->value()?->isSvg() ?? false))
-                        @responsive($media->value(), ['class' => 'max-md::mr-auto relative h-[360px] rounded-bl-2xl object-cover object-left-top md:ml-auto md:h-[720px] md:rounded-b-2xl md:object-right-top'])
+                        @responsive($media->value(), compact('class'))
                     @elseif(($media?->value()?->isVideo() ?? false) && ($media?->value()?->url() ?? false))
-                        <video src="{{ $media->value()->url() }}" loop autoplay muted class="max-md::mr-auto relative h-[360px] rounded-bl-2xl object-cover object-left-top md:ml-auto md:h-[720px] md:rounded-b-2xl md:object-right-top" />
+                        <video src="{{ $media->value()->url() }}" loop autoplay muted :class="$class" />
                     @endif
                 </div>
             </div>
