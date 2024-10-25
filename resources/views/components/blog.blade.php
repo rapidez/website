3ee919b52251c8ab722350d50404c3717df67b28
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-10-25 10:50:04 +0200

    Merge branch 'feature/dynamic-repo-count' of https://github.com/rapidez/website into feature/project-cleanup

--- resources/views/components/blog.blade.php
+++ resources/views/components/blog.blade.php
@@ -1,13 +1,13 @@
 @props(['title', 'shortDescription', 'category', 'image' => null])
 <div class="flex flex-col gap-8">
-    @if($image ?? false)
+    @if($image)
         @responsive($image, ['class' => 'rounded-lg object-cover', 'loading' => 'lazy'])
     @endif
     <div class="flex flex-col self-center">
-        @if($category ?? false)
+        @if($category)
             <span class="text-sm text-secondary-100">{{ $category }}</span>
         @endif
-        @if($title ?? false)
+        @if($title)
                 <h3 class="mt-2 text-2xl font-semibold text-cyan-900">{{ $title }}</h3>
         @endif
         @if($shortDescription)
