60d75393f3bb66b103d810b468ee6ec95a139287
Roy Duineveld <royduineveld@gmail.com>
2025-01-08 13:13:00 +0100

    Smaller blog for images

--- resources/views/page-builder/content.blade.php
+++ resources/views/page-builder/content.blade.php
@@ -1,5 +1,5 @@
 @if ($text)
-    <div class="component container mx-auto max-w-7xl px-6">
+    <div class="component container mx-auto max-w-2xl px-6">
         <div class="prose max-w-none pb-10">
             {!! $text !!}
         </div>
