10ba3cd0fc4633b7ddac3de579cfa70d201a5bb5
Roy Duineveld <royduineveld@gmail.com>
2025-06-05 09:45:24 +0200

    Update site.blade.php

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'Rapidez')</title>
-    <meta name="description" content="@yield('description', 'Headless Magento - with Laravel, Vue and Reactive Search')">
+    <meta name="description" content="@yield('description', 'Headless Magento - with Laravel and InstantSearch')">
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
